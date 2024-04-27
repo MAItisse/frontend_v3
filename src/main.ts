09d6615b0c9528a0f40de62a01755dcecba0f516
vivax3794 <github@vivax3794.com>
2024-04-27 22:41:15 +0200

    add matrial design icons

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import mdiVue from 'mdi-vue/v3'
+import * as mdijs from '@mdi/js'
+
+
 const app = createApp(App)
 
 app.use(router)
+app.use(mdiVue, {
+    icons: mdijs
+})
 
 app.mount('#app')
