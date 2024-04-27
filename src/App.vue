6ac258d0280d97e95b129b027cc977469799df5e
vivax3794 <github@vivax3794.com>
2024-04-27 21:42:14 +0200

    Add simple NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
+    <NavBar />
     <RouterView />
 </template>
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue';
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <img src="/favicon.png" />
+        <h1>JifForgeAi</h1>
+    </header>
+</template>
+
+<style scoped>
+header {
+    width: 100%;
+    background-color: var(--color-background-soft);
+
+    padding: 10px;
+    box-shadow: 10px 0px 20px black;
+    display: inline-flex;
+    height: 5rem;
+
+    align-items: center;
+}
+
+img {
+    margin-right: 20px;
+    height: 4.5rem;
+}
+
+h1 {
+    color: var(--color-heading);
+
+}
+</style>
