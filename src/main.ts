1483e59e0ef9f4904af75f98a91f1d3475ae77aa
vivax3794 <github@vivax3794.com>
2024-04-27 23:11:29 +0200

    Add and setup pinia

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,12 @@ import router from './router'
 
 import mdiVue from 'mdi-vue/v3'
 import * as mdijs from '@mdi/js'
-
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(createPinia())
 app.use(mdiVue, {
     icons: mdijs
 })
