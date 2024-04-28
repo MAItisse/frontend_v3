f604e36c9029628bad700251bbf3ad2b0ea5a506
vivax3794 <github@vivax3794.com>
2024-04-28 05:05:20 +0200

    add lazyloading library for images

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,15 @@ import router from './router'
 import mdiVue from 'mdi-vue/v3'
 import * as mdijs from '@mdi/js'
 import { createPinia } from 'pinia'
+import VueLazyload from 'vue-lazyload'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(createPinia())
+app.use(VueLazyload, {
+    loading: "/loading.gif"
+});
 app.use(mdiVue, {
     icons: mdijs
 })
