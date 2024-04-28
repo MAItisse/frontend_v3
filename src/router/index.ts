07412bcb0b9c1c239d9f97b5497e4f841fa88503
vivax3794 <github@vivax3794.com>
2024-04-28 04:03:37 +0200

    Animated logo on about page

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
+        {
+            path: "/",
+            component: () => import("@/views/About.vue"),
+        }
     ]
 })
 
---
+++ src/views/About.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="logo">
+        M<span id="ai">AI</span>tisse
+    </div>
+</template>
+
+<style scoped>
+#logo {
+    font-size: 8rem;
+    width: 100%;
+    color: var(--c-header);
+    text-align: center;
+    margin-top: 13rem;
+
+    text-shadow: 3px 3px 10px black;
+    transition: all 0.5s;
+}
+
+#ai {
+    display: inline-block;
+    color: var(--c-primary);
+    rotate: 0deg;
+    font-size: 9rem;
+
+    transition: all 0.8s;
+}
+
+#logo:hover #ai {
+    margin-left: 20px;
+    margin-right: 20px;
+
+    background: linear-gradient(90deg, red, orange);
+    background-clip: text;
+    color: transparent;
+    text-shadow: none;
+
+    font-size: 12rem;
+    rotate: 370deg;
+
+    animation: hue_shift 2s infinite linear;
+}
+
+@keyframes hue_shift {
+    0% {
+        filter: hue-rotate(0deg);
+    }
+
+    100% {
+        filter: hue-rotate(360deg);
+    }
+}
+</style>
