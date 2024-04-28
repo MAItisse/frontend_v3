5bb2cafe65a737c405ab6db93d28edb5ce4820c6
vivax3794 <github@vivax3794.com>
2024-04-28 05:05:40 +0200

    create basic user page
    only loads and shows all images user has, for now

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,3 +1,51 @@
 <template>
-    <h1>The amazing user page :P</h1>
+    <img v-for="image in images" v-lazy="image" />
 </template>
+
+<script setup lang="ts">
+import { useDiscord } from '@/stores/discord';
+import { ref, watch, type Ref } from 'vue';
+
+let discord = useDiscord();
+
+const imagesDataUrl = "https://deepnarrationapi.matissetec.dev/getUserImages"
+
+const imageFilter = ref({
+    createImage: true,
+    similarImages: true,
+    combineImage: true,
+    rethemeImage: true,
+    outpaintImage: true,
+    stickerBomb: true,
+    cannyFaceswap: true,
+    qrCode: true,
+    imageBackgroundRemoval: true,
+});
+
+function filter_to_comma(filter: { [key: string]: boolean }): string {
+    return Object.entries(filter).filter(([_, val]) => val).map(([key, _]) => key).join(",");
+}
+
+const images: Ref<string[]> = ref([]);
+
+async function load_images() {
+    let url = `${imagesDataUrl}/${discord.info.user_id}?filter=${filter_to_comma(imageFilter.value)}`;
+    let result = await fetch(url);
+    let data = await result.json();
+
+    images.value = data;
+}
+
+watch(() => discord.dataLoaded, () => {
+    if (discord.dataLoaded) {
+        load_images();
+    }
+}, { immediate: true })
+</script>
+
+<style scoped>
+img {
+    height: 300px;
+    display: block;
+}
+</style>
