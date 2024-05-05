44dfee1a5b9c93bc94b4b8224d18dcaf171dc92d
vivax3794 <github@vivax3794.com>
2024-05-05 22:06:00 +0200

    Add workflow input interfaces

--- src/workflows.ts
+++ src/workflows.ts
@@ -1,18 +1,36 @@
+export interface TextInput {
+    type: "text",
+    placeholder: string,
+}
+
+export type InputType = TextInput;
+
+export interface InputData {
+    key: string,
+    name: string,
+}
+
+type Input = InputData & InputType;
+
 export interface Workflow {
     name: string,
     icon: string,
     endpoint: string,
+    inputs: Input[]
 }
 
 export const WORKFLOWS: { [name: string]: Workflow } = {
     "create": {
         name: "Create Image",
         icon: "camera-plus",
-        endpoint: "/TODO"
-    },
-    "retheme": {
-        name: "Retheme Image",
-        icon: "theme-light-dark",
-        endpoint: "/TODO"
+        endpoint: "startCreateImage",
+        inputs: [
+            {
+                name: "Prompt",
+                key: "prompt",
+                type: "text",
+                placeholder: "forest landscape with birds"
+            }
+        ]
     }
 }
