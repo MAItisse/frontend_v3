863e38685cc8c8ace23c5f81daa2621095066763
vivax3794 <github@vivax3794.com>
2024-07-05 01:12:40 +0200

    Update contact us page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,28 +1,40 @@
 <template>
-    <div id="mainContent">
+    <div class="content">
         <h1>Contact Us</h1>
         <p>Interested in a custom workflow or want to work with us directly?</p>
         <p>Send us an email at <a href="mailto:matissetec@gmail.com">matissetec@gmail.com</a></p>
     </div>
+
+    <div class="content">
+        <h1>Contributors</h1>
+        <p><a href="https://www.twitch.tv/matissetec">MatisseTec</a> - Project lead & Lead backend developer</p>
+        <p><a href="https://vivax.dev">Viv</a> - Lead frontend developer & Backend consultant</p>
+        <!-- Idk if they want to be included? -->
+        <!-- <p><a href="https://github.com/sorahavok">Sora</a> - Backend developer</p> -->
+    </div>
 </template>
 
 <style scoped>
-    #mainContent {
-        margin: 0 auto;
-        max-width: 800px;
-        padding: 20px;
-    }
-    h1 {
-        font-size: 2.5rem;
-        text-align: center;
-    }
-    p {
-        font-size: 1.5rem;
-    }
-    a {
-        color: var(--c-primary);
-    }
-    a:hover {
-        color: var(--c-primary-dark);
-    }
+.content {
+    margin: 0 auto;
+    max-width: 800px;
+    padding: 20px;
+}
+
+h1 {
+    font-size: 2.5rem;
+    text-align: center;
+}
+
+p {
+    font-size: 1.5rem;
+}
+
+a {
+    color: var(--c-primary);
+}
+
+a:hover {
+    color: var(--c-primary-dark);
+}
 </style>
