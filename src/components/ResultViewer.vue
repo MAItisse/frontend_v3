480b75b0423fc8b18e65834c6655995816b1f9dd
Matisse <MatissesProjects@users.noreply.github.com>
2024-07-20 10:18:43 -0700

    making items selectable for discovery page

--- src/components/ResultViewer.vue
+++ src/components/ResultViewer.vue
@@ -1,28 +1,89 @@
 <template>
+  <div class="image-container">
     <div v-if="props.url.endsWith('obj')">
-        <suspense v-if="visible">
-            <ModelViewer :url="props.url" ref="elem" />
-            <template #fallback>
-                <ResultViewer ref="elem" url="/loading.gif" />
-            </template>
-        </suspense>
-        <div ref="elem" v-else>
-            <ResultViewer url="/loading.gif" />
-        </div>
+      <div :class="['checkbox-overlay', { 'selected': props.selected }]" @click="toggleSelection">
+          <input type="checkbox" :checked="props.selected" />
+      </div>
+      <suspense v-if="visible">
+        <ModelViewer :url="props.url" ref="elem" />
+        <template #fallback>
+          <ResultViewer ref="elem" url="/loading.gif" />
+        </template>
+      </suspense>
+      <div ref="elem" v-else>
+        <ResultViewer url="/loading.gif" />
+      </div>
     </div>
-    <img v-else v-lazy="props.url">
+    <div v-else>
+      <div :class="['checkbox-overlay', { 'selected': props.selected }]" @click="toggleSelection">
+        <input type="checkbox" :checked="props.selected" />
+      </div>
+      <p v-if="props.url && isUrlMatch()" @click="toggleSelection">{{ props.url && props.url.match(/[^/]+$/)[0] }}</p>
+      <img v-else v-lazy="props.url" @click="toggleSelection">
+    </div>
+  </div>
 </template>
-
+  
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useElementVisibility } from '@vueuse/core'
-
-let props = defineProps<{
+  import { ref, defineProps, defineEmits } from 'vue';
+  import { useElementVisibility } from '@vueuse/core';
+  
+  let props = defineProps<{
     url: string,
-}>();
+    selected: boolean,
+  }>();
+  
+  const elem = ref(null);
+  const visible = useElementVisibility(elem);
+  
+  const emit = defineEmits(['select']);
+  const textWorkflow = ['createStory', 'createPoem', 'createBackStory', 'createImageDescription']
+  function isUrlMatch() {
+      return props.url && textWorkflow.some(substring => props.url.includes(substring));
+    }
 
-const elem = ref(null);
-const visible = useElementVisibility(elem);
+  function toggleSelection() {
+    emit('select', props.url);
+  }
 </script>
+  
+<style scoped>
+  .image-container {
+    position: relative;
+    display: inline-block;
+    margin: 10px;
+  }
+  
+  .checkbox-overlay {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    background: rgba(255, 255, 255, 0.7);
+    border-radius: 50%;
+    padding: 5px;
+    display: none;
+  }
+  
+  .image-container:hover .checkbox-overlay,
+  .checkbox-overlay.selected {
+    display: block;
+    border: 2px solid #007BFF; /* Adjust the color as needed */
+    z-index: 1;
+  }
+  
+  img {
+    cursor: pointer;
+    width: 100%;
+    height: auto;
+  }
 
-<style scoped></style>
+  p {
+    cursor: pointer;
+    font-size: 1.5rem;
+    margin: 0;
+    padding: 10px;
+    width: 500px;
+    height: 500px;
+    overflow: auto;
+  }
+</style>
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,27 +1,30 @@
 <template>
-    <div id="filters">
-        <div v-for="(_, key) in filters" :key="key">
-            <input type="checkbox" v-model="filters[key]" :id="key">
-            <label :for="key">{{ key.replace('Images', '').replace('Image', '').replace('ackground', 'g') }}</label>
-        </div>
-        <div>
-            <button @click="selectAllFilters(true)">Select All</button>
-            <button @click="selectAllFilters(false)">Select None</button>
-        </div>
+  <div id="filters">
+    <div v-for="(_, key) in filters" :key="key">
+      <input type="checkbox" v-model="filters[key]" :id="key">
+      <label :for="key">{{ key.replace('Images', '').replace('Image', '').replace('ackground', 'g') }}</label>
     </div>
-    <img src="/loading.gif" v-if="ongoing_fetch !== undefined" class="img" />
-    <ResultViewer v-for="image in results" :key="image" :url="image" class="img" />
+    <div>
+      <button @click="selectAllFilters(true)">Select All</button>
+      <button @click="selectAllFilters(false)">Select None</button>
+    </div>
+  </div>
+  <img src="/loading.gif" v-if="ongoing_fetch !== undefined" class="img" />
+  <div v-if="results.length > 0" id="share-options">
+    <button @click="shareImages">Share Selected Images</button>
+  </div>
+  <ResultViewer v-for="image in results" :key="image" :url="image" :selected="selectedImages.includes(image)" @select="toggleImageSelection" />
 </template>
-
+  
 <script setup lang="ts">
-import { useDiscord } from '@/stores/discord';
-import { ref, watch, type Ref } from 'vue';
-
-let discord = useDiscord();
-
-const resultDataUrl = "https://deepnarrationapi.matissetec.dev/getUserResults"
-
-const filters = ref({
+  import { useDiscord } from '@/stores/discord';
+  import { ref, watch, type Ref } from 'vue';
+  
+  let discord = useDiscord();
+  
+  const resultDataUrl = "https://deepnarrationapi.matissetec.dev/getUserResults"
+  
+  const filters = ref({
     createImage: true,
     similarImages: false,
     combineImage: false,
@@ -35,82 +38,123 @@ const filters = ref({
     alphabetImages: false,
     spinMe: false,
     dancerGif: false,
-    // faceswap: false,
     backgroundExtenderGif: false,
-    // videoBackgroundRemoval: false,
     createModel: false,
-});
-
-function filter_to_comma(filter: { [key: string]: boolean }): string {
+    createStory: false,
+    createPoem: false,
+    createBackStory: false,
+    // createJoke: false,
+    // createOneLiner: false,
+    // createRiddle: false,
+    // createTrivia: false,
+    createImageDescription: false,
+    // createSongIdea: false,
+  });
+  
+  function filter_to_comma(filter: { [key: string]: boolean }): string {
     return Object.entries(filter).filter(([_, val]) => val).map(([key, _]) => key).join(",");
-}
-
-const results: Ref<string[]> = ref([]);
-
-async function load_images() {
+  }
+  
+  const results: Ref<string[]> = ref([]);
+  const selectedImages: Ref<string[]> = ref([]);
+  
+  async function load_images() {
     let url = `${resultDataUrl}/${discord.info.user_id}?filter=${filter_to_comma(filters.value)}`;
     let result = await fetch(url);
     let data = await result.json();
-
+  
     results.value = data;
-}
-
-function selectAllFilters(selectAll: boolean) {
+  }
+  
+  function selectAllFilters(selectAll: boolean) {
     Object.keys(filters.value).forEach(key => {
-        filters.value[key] = selectAll;
+      filters.value[key] = selectAll;
     });
-}
-
-watch(() => discord.dataLoaded, () => {
+  }
+  
+  function toggleImageSelection(image: string) {
+    const index = selectedImages.value.indexOf(image);
+    if (index > -1) {
+      selectedImages.value.splice(index, 1);
+    } else {
+      selectedImages.value.push(image);
+    }
+  }
+  
+  async function shareImages() {
+    // Implement sharing functionality here
+    console.log('Sharing images:', selectedImages.value);
+    for (const image of selectedImages.value) {
+        const data = image.split('/');
+        console.log(data[4] + " " + data[7]+"."+data[8]);
+        await fetch("https://deepnarrationapi.matissetec.dev/setItemPublic", {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                workflowName: data[4],
+                discordId: data[5],
+                jobId: data[6],
+                fileName: data[7]+"."+data[8],
+            })
+        });
+    }
+  }
+  
+  watch(() => discord.dataLoaded, () => {
     if (discord.dataLoaded) {
-        load_images();
+      load_images();
     }
-}, { immediate: true });
-
-let ongoing_fetch: Ref<undefined | number> = ref(undefined);
-watch(filters.value, () => {
+  }, { immediate: true });
+  
+  let ongoing_fetch: Ref<undefined | number> = ref(undefined);
+  watch(filters.value, () => {
     if (ongoing_fetch.value !== undefined) {
-        clearTimeout(ongoing_fetch.value);
+      clearTimeout(ongoing_fetch.value);
     }
-
+  
     ongoing_fetch.value = setTimeout(() => {
-        ongoing_fetch.value = undefined;
-        load_images();
+      ongoing_fetch.value = undefined;
+      // remove urls that are not in the new filter list filters
+      selectedImages.value = selectedImages.value.filter(image => filter_to_comma(filters.value).split(",").some(filter => image.includes(filter)));
+      
+      load_images();
     }, 1000);
-});
+  });
 </script>
-
+  
 <style scoped>
-.img {
+  .img {
     max-width: 100%;
     min-width: 500px;
     height: 500px;
     object-fit: contain;
-
     display: inline-block;
-
     margin: 0px;
     margin-right: 10px;
-}
-
-label {
+  }
+  
+  label {
     font-size: 2rem;
-}
-
-input {
+  }
+  
+  input {
     width: 30px;
     height: 30px;
-}
-
-#filters {
+  }
+  
+  #filters {
     display: grid;
     grid-template-columns: repeat(auto-fill, 500px);
-}
-
-button {
+  }
+  
+  button {
     margin-right: 10px;
     padding: 10px;
     font-size: 1.5rem;
     cursor: pointer;
-}
+  }
+  
+  #share-options {
+    margin-top: 20px;
+  }
 </style>
