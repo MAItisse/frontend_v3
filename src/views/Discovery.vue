9f8c2d720e9ddccbadb2ebf11fb4df64016edb94
Matisse <MatissesProjects@users.noreply.github.com>
2024-07-20 10:17:56 -0700

    adding Discovery page filter

--- src/views/Discovery.vue
+++ src/views/Discovery.vue
@@ -1,6 +1,17 @@
 <template>
     <h1>Discovery</h1>
     <p>See what others have made</p>
+    <!-- <FilterSelector /> -->
+    <div id="filters">
+        <div v-for="(_, key) in filters" :key="key">
+            <input type="checkbox" v-model="filters[key]" :id="key">
+            <label :for="key">{{ key.replace('Images', '').replace('Image', '').replace('ackground', 'g') }}</label>
+        </div>
+        <div>
+            <button @click="selectAllFilters(true)">Select All</button>
+            <button @click="selectAllFilters(false)">Select None</button>
+        </div>
+    </div>
     <img src="/loading.gif" v-if="ongoing_fetch !== undefined" class="img" />
     <ResultViewer v-for="image in results" :key="image" :url="image" class="img" />
 </template>
@@ -18,29 +29,48 @@
 
     const filters = ref({
         createImage: true,
-        // similarImages: false,
-        // combineImage: false,
-        // rethemeImage: false,
-        // relightImage: false,
-        // outpaintImage: false,
-        // stickerBomb: false,
-        // cannyFaceswap: false,
-        // qrCode: false,
-        // imageBackgroundRemoval: false,
-        // alphabetImages: false,
-        // spinMe: false,
-        // dancerGif: false,
-        // backgroundExtenderGif: false,
-        // // videoBackgroundRemoval: false,
-        // createModel: false,
+        similarImages: false,
+        combineImage: false,
+        rethemeImage: false,
+        relightImage: false,
+        outpaintImage: false,
+        stickerBomb: false,
+        cannyFaceswap: false,
+        qrCode: false,
+        imageBackgroundRemoval: false,
+        alphabetImages: false,
+        spinMe: false,
+        dancerGif: false,
+        backgroundExtenderGif: false,
+        // videoBackgroundRemoval: false,
+        createModel: false,
+        createStory: false,
+        createPoem: false,
+        createBackStory: false,
+        // createJoke: false,
+        // createOneLiner: false,
+        // createRiddle: false,
+        // createTrivia: false,
+        createImageDescription: false,
+        // createSongIdea: false,
     });
 
     async function load_images() {
-        let url = `${resultDataUrl}?filter=createImage`; //${filter_to_comma(filters.value)}
+        let url = `${resultDataUrl}?filter=${filter_to_comma(filters.value)}`;
         let result = await fetch(url);
         let data = await result.json();
 
         results.value = data;
+    }
+
+    function filter_to_comma(filter: { [key: string]: boolean }): string {
+        return Object.entries(filter).filter(([_, val]) => val).map(([key, _]) => key).join(",");
+    }
+
+    function selectAllFilters(selectAll: boolean) {
+        Object.keys(filters.value).forEach(key => {
+            filters.value[key] = selectAll;
+        });
     }
 
     watch(filters.value, () => {
@@ -61,4 +91,24 @@
 </script>
 
 <style scoped>
+label {
+    font-size: 2rem;
+}
+
+input {
+    width: 30px;
+    height: 30px;
+}
+
+#filters {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, 500px);
+}
+
+button {
+    margin-right: 10px;
+    padding: 10px;
+    font-size: 1.5rem;
+    cursor: pointer;
+}
 </style>
